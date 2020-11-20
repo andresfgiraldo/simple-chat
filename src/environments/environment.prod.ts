6d99d0e5a3e78c6de52f02b3a0e4eda4d72512e9
Andres Giraldo <andresfgiraldo@live.com>
2020-11-19 22:54:59 -0500

    environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCEkrfjTMG79Rv6gKQslCLHnSd9cW6_-FU",
+    authDomain: "chat-angular-fire.firebaseapp.com",
+    databaseURL: "https://chat-angular-fire.firebaseio.com",
+    projectId: "chat-angular-fire",
+    storageBucket: "chat-angular-fire.appspot.com",
+    messagingSenderId: "702678574375",
+    appId: "1:702678574375:web:437a85e0f2b48c59dfb502",
+    measurementId: "G-1Y1YX87E8N"
+  }
 };
